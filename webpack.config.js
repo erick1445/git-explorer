6896c47098d76091d4ee5c5db6692da05ce7f27e
Erick <kekierick15@gmail.com>
2022-05-30 17:38:40 -0300

    etruturando o React

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
 
 module.exports = {
+    mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
-       path: path.resolve(__dirname, 'dits'),
+       path: path.resolve(__dirname, 'dist'),
        filename: 'bundle.js'
     },
     resolve: {
