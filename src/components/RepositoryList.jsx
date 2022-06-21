04108a7a3ce385f8fb5d63a64d05066044c4aa91
Erick <kekierick15@gmail.com>
2022-06-21 08:28:41 -0300

    add arquivo de estilização

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss';
+
 const repository = {
     name: 'unform',
     description: 'Forms in React',
@@ -11,14 +13,18 @@ const repositoryName = 'unform2'
 export function RepositoryList() {
     return (
       <section className="repository-list">
+          
           <h1>Lista de Repositórios</h1>
 
           <ul>
+          
           <RepositoryItem repository={repository}/>
           <RepositoryItem repository={repository}/>
           <RepositoryItem repository={repository}/>
           <RepositoryItem repository={repository}/>
+          
           </ul>
+      
       </section>
     );
 };
